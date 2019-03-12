eb81473b9dc5b3173063f9db205029ed0278f010
Efe Emir Pekmez <efeemir103@gmail.com>
2019-03-12 13:39:50 +0300

    Update data_types.c

--- source/teminal_test/data_types.c
+++ source/teminal_test/data_types.c
@@ -1,4 +1,4 @@
-//Dont define stdio here we will include this library under stdio & stdlib & time under main source code.
+//Dont define stdio here we will include this library under stdio & stdlib & string & time under main source code.
 typedef struct{
   char * text;
   long int timestamp;
@@ -12,5 +12,18 @@ typedef struct{
   char * username;
   message_list database;
 }server;
-
-  
+typedef struct{
+  int year;
+  int month;
+  int day;
+  int hour;
+  int minute;
+  int second;
+}t_;
+t_ get_time(time_t timestamp){
+  char * tmp=ctime(&timestamp);
+  char tmpmonth[4];
+  t_ res;
+  sscanf(tmp,"%*c%*c%*c %*c%*c%*c %d %d:%d:%d %d",&day,&hour,&minute,&second,&year);
+  sscanf(tmp,"%*c%*c%*c %c%c%c %*d %*d:%*d:%*d %*d",&tmpmonth[0],&tmpmonth[1],&tmpmonth[2]);
+  if(!strcmp(tmpmonth,"")//write months here
